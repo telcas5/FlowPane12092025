6b2999be10d28daae224a15e747f8eb78fe5b2bb
telcaslor <thatbytc@gmail.com>
2025-09-14 22:07:23 +0200

    Tercer commit con el css cambiado

--- src/main/java/es/telmocas/App.java
+++ src/main/java/es/telmocas/App.java
@@ -1,43 +1,33 @@
 // FlowPaneAlignment.java
 package es.telmocas;
+
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
+
     @Override
-    public void start(Stage stage) {
-        FlowPane fp1 = createFlowPane(Pos.BOTTOM_RIGHT);
-        FlowPane fp2 = createFlowPane(Pos.BOTTOM_LEFT);
-        FlowPane fp3 = createFlowPane(Pos.CENTER);
-        HBox root = new HBox(fp1, fp2, fp3);
+    public void start(Stage stage) throws Exception {
+        // Cargar el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista.fxml"));
+        Parent root = loader.load();
+
+        // Crear la escena
         Scene scene = new Scene(root);
+
+        // Cargar el CSS
+        scene.getStylesheets().add(getClass().getResource("/estilo.css").toExternalForm());
+
+        // Configurar y mostrar la ventana
+        stage.setTitle("Mi Aplicación JavaFX");
         stage.setScene(scene);
-        stage.setTitle("FlowPane Alignment");
         stage.show();
     }
-    private FlowPane createFlowPane(Pos alignment) {
-        FlowPane fp = new FlowPane(5, 5);
-        fp.setPrefSize(200, 100);
-        fp.setAlignment(alignment);
-        fp.getChildren().addAll(new Text(alignment.toString()),
-                new Button("Button 1"),
-                new Button("Button 2"),
-                new Button("Button 3"));
-        fp.setStyle("-fx-padding: 10;" +
-                "-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;" +
-                "-fx-border-insets: 5;" +
-                "-fx-border-radius: 5;" +
-                "-fx-border-color: blue;");
-        return fp;
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
